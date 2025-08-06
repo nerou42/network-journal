df26f5dbd0c6a2c44ed3966f8fcbad49064338b8
Andreas Wahlen <andreas.wahlen@nerou.de>
2025-08-06 19:16:44 +0200

    Fix NetworkError parsing: url is optional and sampling_fraction is float

--- src/nel.rs
+++ src/nel.rs
@@ -22,7 +22,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::{Report, ReportType};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
 #[serde(rename_all = "snake_case")]
 pub enum Phase {
     #[serde(rename = "dns")]
@@ -31,7 +31,7 @@ pub enum Phase {
     Application
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct NetworkError {
     elapsed_time: u64,
     method: String,
@@ -39,11 +39,12 @@ pub struct NetworkError {
     protocol: String,
     #[serde(skip_serializing_if = "Option::is_none")]
     referrer: Option<String>,
-    sampling_fraction: u16,
+    sampling_fraction: f32,
     server_ip: String,
     status_code: u16,
     r#type: String,
-    url: String
+    #[serde(skip_serializing_if = "Option::is_none")]
+    url: Option<String>
 }
 
 pub async fn report_nel(report: Json<Report<NetworkError>>) -> impl Responder {
@@ -53,5 +54,53 @@ pub async fn report_nel(report: Json<Report<NetworkError>>) -> impl Responder {
     } else {
         error!("invalid report type: {:?}", report.r#type);
         HttpResponse::BadRequest()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_report_ex3() {
+        // source: https://www.w3.org/TR/network-error-logging/
+        let json = r#"{
+            "age": 0,
+            "type": "network-error",
+            "url": "https://www.example.com/",
+            "body": {
+                "sampling_fraction": 0.5,
+                "referrer": "http://example.com/",
+                "server_ip": "2001:DB8:0:0:0:0:0:42",
+                "protocol": "h2",
+                "method": "GET",
+                "request_headers": {},
+                "response_headers": {},
+                "status_code": 200,
+                "elapsed_time": 823,
+                "phase": "application",
+                "type": "http.protocol.error"
+            }
+        }"#;
+        let res = serde_json::from_str::<Report<NetworkError>>(json);
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), Report {
+            r#type: ReportType::NetworkError,
+            body: NetworkError {
+                elapsed_time: 823,
+                method: "GET".to_string(),
+                phase: Phase::Application,
+                protocol: "h2".to_string(),
+                referrer: Some("http://example.com/".to_string()),
+                sampling_fraction: 0.5,
+                server_ip: "2001:DB8:0:0:0:0:0:42".to_string(),
+                status_code: 200,
+                r#type: "http.protocol.error".to_string(),
+                url: None
+            },
+            age: Some(0),
+            url: "https://www.example.com/".to_string(),
+            user_agent: None,
+        })
     }
 }
